511c9041f526a1fb5e79dc7fe39a13abdcd72208
sejal-27 <sejall2709@gmail.com>
2024-03-28 18:21:57 +0530

    testing animations

--- sky-sight-app/src/components/weather.jsx
+++ sky-sight-app/src/components/weather.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Weather = () => {
   const [weather, setWeather] = useState(null);
@@ -10,8 +11,8 @@ const Weather = () => {
     async function fetchData() {
       try {
         const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
-        ;
+          `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
+        );
         setWeather(response.data);
       } catch (error) {
         console.error("Error fetching data", error);
@@ -26,11 +27,25 @@ const Weather = () => {
         <div className="">
           <div className=""> Weather in {weather.name}</div>
           <div className=""> Temperature: {weather.main.temp}°C</div>
-          <div className=""> feels like Temperature: {weather.main.feels_like}°C</div>
+          <div className="">
+            {" "}
+            feels like Temperature: {weather.main.feels_like}°C
+          </div>
           <div className=""> Condition: {weather.weather[0].description}</div>
           <div className=""> Humidity:{weather.main.humidity}</div>
           <div className=""> Visibility:{weather.visibility} mtr</div>
           <div className=""> Wind:{weather.wind.speed} m/s</div>
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 0.2 }} transition={{duration:0.5 , delay:0.5}}>
+            I have some content in here
+          </motion.div>
+          <AnimatePresence>
+            <motion.div exit={{ x: "-100vh", opacity: 0 }}>
+              Watch me go woosh!
+            </motion.div>
+          </AnimatePresence>
+<motion.div whileHover={{height:"4em", width:"5em", backgroundColor:"wheat"}} style={{height:"2em", width:"2em", backgroundColor:"red"}}>
+hi
+</motion.div>
 
         </div>
       )}
