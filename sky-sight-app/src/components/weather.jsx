9ab9da43b974ec7c3f0b5b5a21ba7fe8e1c37bcd
sejal-27 <sejall2709@gmail.com>
2024-03-30 11:18:54 +0530

    adding icon from api

--- sky-sight-app/src/components/weather.jsx
+++ sky-sight-app/src/components/weather.jsx
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 // import { motion, AnimatePresence } from "framer-motion";
 
-const Weather = ({cityName}) => {
+const Weather = ({ cityName }) => {
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState(null);
   const apiKey = "2b2e87bfb47de09513bdee9d38f3009b";
-  console.log(cityName,"222");
+  console.log(cityName, "222");
 
   useEffect(() => {
     async function fetchData() {
       try {
-        console.log(cityName,"11111");
-        if(!cityName)return;
+        console.log(cityName, "11111");
+        if (!cityName) return;
         const response = await axios.get(
           `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
         );
@@ -22,7 +22,7 @@ const Weather = ({cityName}) => {
         setError("Error fetching weather data");
         setWeather(null);
       }
-    };
+    }
     fetchData();
   }, [cityName, apiKey]);
 
@@ -31,7 +31,7 @@ const Weather = ({cityName}) => {
       {error && <p>{error}</p>}
       {weather && (
         <div className="">
-          <div className=""> Weather in {weather.name}</div>
+          <div className="location">{weather.name}</div>
           <div className=""> Temperature: {weather.main.temp}°C</div>
           <div className="">
             {" "}
@@ -41,6 +41,13 @@ const Weather = ({cityName}) => {
           <div className=""> Humidity:{weather.main.humidity}</div>
           <div className=""> Visibility:{weather.visibility} mtr</div>
           <div className=""> Wind:{weather.wind.speed} m/s</div>
+          <div>
+            Icon:
+            <img
+              src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+              alt="Weather Icon"
+            />
+          </div>
           {/* <motion.div initial={{ opacity: 0 }} animate={{ opacity: 0.2 }} transition={{duration:0.5 , delay:0.5}}>
             I have some content in here
           </motion.div>
